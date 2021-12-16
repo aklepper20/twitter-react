560d8b1dbc498ec66ffb24d570df789a4634e8a9
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-15 20:56:52 -0800

    adds animation to adding of tweets

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,6 +3,7 @@ import TweetBox from "./TweetBox";
 import Post from "./Post";
 import "../css/Feed.css";
 import db from "../firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -19,18 +20,22 @@ function Feed() {
         <h2>Home</h2>
       </div>
       <TweetBox />
-      {posts.map((post) => {
-        return (
-          <Post
-            displayName={post.displayName}
-            username={post.username}
-            verified={post.verified}
-            text={post.text}
-            avatar={post.avatar}
-            image={post.image}
-          />
-        );
-      })}
+
+      <FlipMove>
+        {posts.map((post) => {
+          return (
+            <Post
+              key={post.text}
+              displayName={post.displayName}
+              username={post.username}
+              verified={post.verified}
+              text={post.text}
+              avatar={post.avatar}
+              image={post.image}
+            />
+          );
+        })}
+      </FlipMove>
     </div>
   );
 }
--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/TweetBox.css";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
+import db from "../firebase";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "qazi",
+      username: "cleverqazi",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar: "https://lwlies.com/wp-content/uploads/2017/04/avatar-2009.jpg",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar alt="User Profile Image" />
-          <input placeholder="What's going on?" type="text" />
+          <input
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+            placeholder="What's going on?"
+            type="text"
+          />
         </div>
         <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
           className="tweetBox__imageInput"
           placeholder="Enter image Url"
           type="text"
         />
-        <Button className="tweetBox__tweetButton">Tweet</Button>
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
       </form>
     </div>
   );
