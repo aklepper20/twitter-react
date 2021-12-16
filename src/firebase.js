dc97bb0feb7da4f7252ff8f9ea976151b6f6bc3b
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2021-12-15 19:56:44 -0800

    completes widget component and styling

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO3laWkCNnZBASBDNgNu7jTLWGdHhQghk",
+  authDomain: "twitter-5fda1.firebaseapp.com",
+  projectId: "twitter-5fda1",
+  storageBucket: "twitter-5fda1.appspot.com",
+  messagingSenderId: "528102199822",
+  appId: "1:528102199822:web:cbc8d2d5e621ece6bc625f",
+  measurementId: "G-T6S3KGS3T7",
+};
